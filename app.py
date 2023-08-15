0460e90c07b0cfb4993498e1dd820a786a4cb120
flying-charlie <59337282+flying-charlie@users.noreply.github.com>
2023-08-15 13:03:32 +0000

    Tue, Aug 15, 2023, 2:03 PM +01:00

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def new():
 
         db.execute("INSERT INTO progressions (user_id, name, time_signature) VALUES (?,?,?)", session.get("user_id"), request.form.get("name"), request.form.get("time_signature"))
 
-        return redirect("/edit")
+        return redirect(url_for('.edit', messages=messages))
     else:
         return render_template("new.html")
 
