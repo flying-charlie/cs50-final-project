a3502e891043854ee4f90c5258a2c3c0db97c612
flying-charlie <59337282+flying-charlie@users.noreply.github.com>
2023-08-15 12:49:48 +0000

    Tue, Aug 15, 2023, 1:49 PM +01:00

--- app.py
+++ app.py
@@ -49,6 +49,12 @@ def new():
     """create new project"""
     #TODO
     if request.method == "POST":
+        if not request.form.get("name"):
+            return apology("must provide progression name", 403)
+
+        elif not request.form.get("password"):
+            return apology("must provide a time signature (_/4)", 403)
+
         return redirect("/edit")
     else:
         return render_template("new.html")
