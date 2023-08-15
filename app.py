2ac0409aee032a25ae3332f13d9411f977b9e5cf
flying-charlie <59337282+flying-charlie@users.noreply.github.com>
2023-08-15 13:13:53 +0000

    Tue, Aug 15, 2023, 2:13 PM +01:00

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 
-from sqlite3 import url_for
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
