e217f498b3f42135e95409a07ba33dcea60400d0
flying-charlie <59337282+flying-charlie@users.noreply.github.com>
2023-08-16 10:18:15 +0000

    Wed, Aug 16, 2023, 11:18 AM +01:00

--- app.py
+++ app.py
@@ -54,6 +54,9 @@ def new():
         elif (not request.form.get("time_signature")) or (not is_int(request.form.get("time_signature"))) or (int(request.form.get("time_signature")) < 1):
             return apology("must provide a time signature (_/4)", 403)
 
+        elif (not request.form.get("tempo")) or (not is_int(request.form.get("tempo"))) or (int(request.form.get("tempo")) < 1):
+            return apology("must provide a tempo (beats per minute)", 403)
+
         elif db.execute("SELECT id FROM progressions WHERE user_id = ? AND name = ?", session.get("user_id"), request.form.get("name")):
             return apology("There is already a progression whith that name", 403)
 
