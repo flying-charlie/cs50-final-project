66b818648187cf8725317ea801e1cc3693130637
flying-charlie <59337282+flying-charlie@users.noreply.github.com>
2023-08-16 10:33:36 +0000

    Wed, Aug 16, 2023, 11:33 AM +01:00

--- app.py
+++ app.py
@@ -49,16 +49,16 @@ def new():
     """create new project"""
     if request.method == "POST":
         if not request.form.get("name"):
-            return apology("must provide progression name", 403)
+            return apology("must provide progression name", "/new")
 
         elif (not request.form.get("time_signature")) or (not is_int(request.form.get("time_signature"))) or (int(request.form.get("time_signature")) < 1):
-            return apology("must provide a time signature (_/4)", 403)
+            return apology("must provide a time signature (_/4)", "/new")
 
         elif (not request.form.get("tempo")) or (not is_int(request.form.get("tempo"))) or (int(request.form.get("tempo")) < 1):
-            return apology("must provide a tempo (beats per minute)", 403)
+            return apology("must provide a tempo (beats per minute)", "/new")
 
         elif db.execute("SELECT id FROM progressions WHERE user_id = ? AND name = ?", session.get("user_id"), request.form.get("name")):
-            return apology("There is already a progression with that name", 403)
+            return apology("There is already a progression with that name", "/new")
 
         db.execute("INSERT INTO progressions (user_id, name, time_signature) VALUES (?,?,?)", session.get("user_id"), request.form.get("name"), request.form.get("time_signature"))
         id = db.execute("SELECT id FROM progressions WHERE user_id = ? AND name = ? AND time_signature = ?", session.get("user_id"), request.form.get("name"), request.form.get("time_signature"))
@@ -95,18 +95,18 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", "/login")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", "/login")
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid username and/or password", "/login")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -138,21 +138,21 @@ def register():
     if request.method == "POST":
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 400)
+            return apology("must provide username", "/register")
 
         if len(db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))) != 0:
-            return apology("that username is already taken", 400)
+            return apology("that username is already taken", "/register")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 400)
+            return apology("must provide password", "/register")
 
         # Ensure confirmation was submitted
         elif not request.form.get("confirmation"):
-            return apology("must provide password confirmation", 400)
+            return apology("must provide password confirmation", "/register")
 
         elif request.form.get("password") != request.form.get("confirmation"):
-            return apology("Password must be the same as the password confirmation", 400)
+            return apology("Password must be the same as the password confirmation", "/register")
 
         db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), generate_password_hash(request.form.get("password")))
 
